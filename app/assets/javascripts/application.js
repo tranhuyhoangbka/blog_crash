fdcd8db38840f35db943b3fae7e0cf6df7f3be04
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-10-12 12:57:52 +0700

    simple form demo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
+//= require jquery.timepicker.js
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007063423) do
+ActiveRecord::Schema.define(version: 20151012060525) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -85,6 +85,17 @@ ActiveRecord::Schema.define(version: 20151007063423) do
   end
 
   add_index "posts", ["author_id"], name: "index_posts_on_author_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",    limit: 255
+    t.string   "password",    limit: 255
+    t.string   "email",       limit: 255
+    t.boolean  "remember_me"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.text     "intro",       limit: 65535
+    t.datetime "birthday"
+  end
 
   create_table "venues", force: :cascade do |t|
     t.string   "name",       limit: 255
